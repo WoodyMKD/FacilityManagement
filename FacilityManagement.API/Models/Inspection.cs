17ff19d70eee3123fad509dca12b9389d63efc0d
Методија Новковски <Metodija.Novkovski@students.finki.ukim.mk>
2019-09-19 11:55:04 +0200

    Added Inspection class

---
+++ FacilityManagement.API/Models/Inspection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FacilityManagement.API.Models
+{
+    public class Inspection
+    {
+        [Key]
+        public int InspectionId { get; set; }
+        public virtual InventoryObject Object { get; set; }
+        public virtual InventoryObjectType Type { get; set; }
+        public virtual ICollection<InventoryObjectPart> Parts { get; set; }
+        public DateTime Date { get; set; }
+        public virtual ICollection<bool> Functional { get; set; }
+    }
+}
